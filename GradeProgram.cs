efe14e20e4746ebb8a3ca801f47a2668069f074c
tandoh22 <tandohsamuel4u@gmail.com>
2025-08-05 09:53:48 +0000

    grade program

--- GradeProgram.cs
+++ GradeProgram.cs
@@ -8,26 +8,26 @@ namespace Dcit318_assignment1_11108240
         {
             Console.WriteLine("Enter your score (0 - 100): ");
 
-            if (!double.TryParse(Console.ReadLine(), out double grade))
+            if (!double.TryParse(Console.ReadLine(), out double score))
             {
                 Console.WriteLine("Invalid input! Please enter a number.");
                 return;
             }
 
-            if (grade < 0 || grade > 100)
+            if (score < 0 || score > 100)
             {
                 Console.WriteLine("Error: Please enter a score between 0 and 100.");
                 return;
             }
 
             string letterGrade;
-            if (grade >= 90)
+            if (score >= 90)
                 letterGrade = "A";
-            else if (grade >= 80)
+            else if (score >= 80)
                 letterGrade = "B";
-            else if (grade >= 70)
+            else if (score >= 70)
                 letterGrade = "C";
-            else if (grade >= 60)
+            else if (score >= 60)
                 letterGrade = "D";
             else
                 letterGrade = "F";
--- obj/Debug/net9.0/Dcit318-assignment1-11108240.AssemblyInfo.cs
+++ obj/Debug/net9.0/Dcit318-assignment1-11108240.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dcit318-assignment1-11108240")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02145276efacfc980c5855bb7b48c2603933bfd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5405acfa6300d83a7d76d8827524dccd54780394")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dcit318-assignment1-11108240")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dcit318-assignment1-11108240")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
